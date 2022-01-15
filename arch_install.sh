ac038fec5c98221ef0529ea85f4b7fc3735ba272
Philippe Olivier <philippe.olivier@polymtl.ca>
2022-01-15 12:17:18 -0500

    ext4

--- arch_install.sh
+++ arch_install.sh
@@ -7,19 +7,15 @@
 # Introduction
 # ============
 #
-# The following is a basic Arch Linux installation script that provides the following:
-# - Btrfs filesystem
-# - LUKS2 encryption
-# - GRUB bootloader that handles bootable snapshots
+# The following is a simplistic Arch Linux installation script that provides the following:
+# - Single-volume LUKS encryption with ext4
+# - Booting is managed with systemd-boot
 #
 # As always, make sure to validate this script against the current official installation guide that
 # can be found on the Arch Wiki, to ensure that the installation proceeds reliably.
 #
-# This script is a heavily based off the work of Tommaso Chiti. His script can be found here:
+# Many parts of this script are based off the work of Tommaso Chiti. His script can be found here:
 # https://github.com/classy-giraffe/easy-arch
-#
-# This script is a stripped-down version of Tommaso's script, custom tailored to my specific needs.
-# If you want a more general script, you should definitely use Tommaso's script.
 #
 #
 # Usage
@@ -38,25 +34,8 @@
 # | # | Label | Size    | Mountpoint | Filesystem |
 # +---+-------+---------+------------+------------+
 # | 1 | ESP   | 512 MiB | /boot      | FAT32      |
-# | 2 | ROOT  | Rest    | /          | Btrfs      |
+# | 2 | ROOT  | Rest    | /          | Ext4       |
 # +---+-------+---------+------------+------------+
-#
-#
-# Btrfs subvolumes layout
-# =======================
-#
-# The following layout is used, as suggested for Snapper here:
-# https://wiki.archlinux.org/title/Snapper#Suggested_filesystem_layout
-#
-# +------------+-----------------------+
-# | Name       | Mountpoint            |
-# +------------+-----------------------+
-# | @          | /                     |
-# | @home      | /home                 |
-# | @snapshots | /.snapshots           |
-# | @var_log   | /var/log              |
-# | @var_pkgs  | /var/cache/pacman/pkg |
-# +------------+-----------------------+
 
 
 USERNAME="pholi"
@@ -124,7 +103,7 @@ parted -s "${DEVICE}" \
     mklabel gpt \
     mkpart ${BOOT_LABEL} fat32 1MiB 513MiB \
     set 1 esp on \
-    mkpart ${ROOT_LABEL} 513MiB 100% \
+    mkpart ${ROOT_LABEL} ext4 513MiB 100% \
 
 # Informing the Kernel of the changes.
 print "Informing the Kernel about the disk changes."
@@ -140,40 +119,18 @@ read -r -s -p "Enter a password for the LUKS container: " LUKS_PASSPHRASE
 echo -n "${LUKS_PASSPHRASE}" | cryptsetup luksFormat --type luks2 "${ROOT_PARTITION}" -d -
 echo -n "${LUKS_PASSPHRASE}" | cryptsetup open --type luks2 "${ROOT_PARTITION}" "${LUKS_MAPPING}" -d -    
 
-# Formatting the LUKS Container as Btrfs.
-print "Formatting the LUKS container as Btrfs."
-mkfs.btrfs ${CRYPTROOT} &>/dev/null
+# Formatting the LUKS Container as Ext4.
+print "Formatting the LUKS container as Ext4."
+mkfs.ext4 ${CRYPTROOT} &>/dev/null
+
+# Mounting the partitions.
 mount ${CRYPTROOT} /mnt
-
-# Creating Btrfs subvolumes.
-print "Creating Btrfs subvolumes."
-for VOLUME in @ @home @snapshots @var_log @var_pkgs
-do
-    btrfs subvolume create /mnt/${VOLUME}
-done
-
-# Mounting the newly created subvolumes.
-umount /mnt
-print "Mounting the newly created subvolumes."
-# Notes:
-# - The flag `nodiratime` is not necessary, it is implied by `noatime`.
-# - The flag `ssd` is not necessary, as Btrfs will automatically detect if a device is an SSD.
-# - The flag `compress-force=zstd:1` is used instead of `compress=zstd:1`, because: https://www.reddit.com/r/archlinux/comments/npppg5/how_do_i_safely_switch_from_lzo_compression_to/
-# - The flag `discard=async` is not used, and instead fstrim is used.
-BTRFS_MOUNT_OPTIONS="noatime,compress-force=zstd:1,autodefrag,subvol="
-# TODO: before changing the mount options to my variable thing, they were: ssd,noatime,compress-force=zstd:3,discard=async,subvol=@home
-mount -o ${BTRFS_MOUNT_OPTIONS}@ ${CRYPTROOT} /mnt
-mkdir -p /mnt/{home,.snapshots,/var/log,/var/cache/pacman/pkg,boot}
-mount -o ${BTRFS_MOUNT_OPTIONS}@home ${CRYPTROOT} /mnt/home
-mount -o ${BTRFS_MOUNT_OPTIONS}@snapshots ${CRYPTROOT} /mnt/.snapshots
-mount -o ${BTRFS_MOUNT_OPTIONS}@var_log ${CRYPTROOT} /mnt/var/log
-mount -o ${BTRFS_MOUNT_OPTIONS}@var_pkgs ${CRYPTROOT} /mnt/var/cache/pacman/pkg
-chattr +C /mnt/var/log
+mkdir /mnt/boot
 mount ${BOOT_PARTITION} /mnt/boot
 
 # Pacstrap (setting up a base sytem onto the new root).
 print "Installing the base system (it may take a while)."
-pacstrap /mnt base base-devel btrfs-progs efibootmgr grub grub-btrfs intel-ucode iwd linux linux-firmware linux-headers rsync snapper snap-pac zram-generator
+pacstrap /mnt base base-devel intel-ucode iwd linux linux-firmware linux-headers
 
 # Setting up the hostname.
 echo "${HOSTNAME}" > /mnt/etc/hostname
@@ -199,14 +156,36 @@ EOF
 
 # Configuring /etc/mkinitcpio.conf.
 print "Configuring /etc/mkinitcpio.conf."
-sed -i "/^MODULES=(/cMODULES=(btrfs)" /mnt/etc/mkinitcpio.conf
-sed -i "/^HOOKS=(/cHOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems)" /mnt/etc/mkinitcpio.conf
-sed -i "/^COMPRESSION=(/cCOMPRESSION=(zstd)" /mnt/etc/mkinitcpio.conf
+sed -i "/^HOOKS=(/cHOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck)" /mnt/etc/mkinitcpio.conf
 
-# Setting up LUKS2 encryption in GRUB.
-print "Setting up GRUB config."
+# Setting up the bootloader.
+print "Setting up systemd-boot."
+bootctl --path=/mnt/boot install
+
+cat > /mnt/boot/loader/loader.conf <<EOF
+default  arch.conf
+timeout  5
+console-mode max
+editor   no
+EOF
+
 UUID=$(blkid -s UUID -o value ${ROOT_PARTITION})
-sed -i "s,quiet,quiet rd.luks.name=${UUID}=${LUKS_MAPPING} root=${CRYPTROOT},g" /mnt/etc/default/grub
+
+cat > /mnt/boot/loader/entries/arch.conf <<EOF
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux.img
+options root=${CRYPTROOT} rd.luks.name=${UUID}=${LUKS_MAPPING} rw
+EOF
+
+cat > /mnt/boot/loader/entries/arch-fallback.conf <<EOF
+title   Arch Linux (fallback initramfs)
+linux   /vmlinuz-linux
+initrd  /intel-ucode.img
+initrd  /initramfs-linux-fallback.img
+options root=${CRYPTROOT} rd.luks.name=${UUID}=${LUKS_MAPPING} rw
+EOF
 
 # Configuring the system.    
 arch-chroot /mnt /bin/bash -e <<EOF
@@ -228,24 +207,6 @@ arch-chroot /mnt /bin/bash -e <<EOF
     # Generating a new initramfs.
     echo "Creating a new initramfs."
     mkinitcpio -P &>/dev/null
-    
-    # Snapper configuration.
-    echo "Configuring Snapper."
-    umount /.snapshots
-    rm -r /.snapshots
-    snapper --no-dbus -c root create-config /
-    btrfs subvolume delete /.snapshots &>/dev/null
-    mkdir /.snapshots
-    mount -a
-    chmod 750 /.snapshots
-    
-    # Installing GRUB.
-    echo "Installing GRUB on /boot."
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB &>/dev/null
-
-    # Creating GRUB config file.
-    echo "Creating GRUB config file."
-    grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
 EOF
 
 # Setting root password.
@@ -258,32 +219,6 @@ arch-chroot /mnt useradd -m -G wheel -s /bin/bash "${USERNAME}"
 echo "${USERNAME} ALL=(ALL) ALL" >> /mnt/etc/sudoers
 print "Setting user password for ${USERNAME}." 
 arch-chroot /mnt /bin/passwd "${USERNAME}"
-
-# Boot backup hook.
-print "Configuring /boot backup when pacman transactions are made."
-mkdir /mnt/etc/pacman.d/hooks
-cat > /mnt/etc/pacman.d/hooks/50-bootbackup.hook <<EOF
-[Trigger]
-Operation = Upgrade
-Operation = Install
-Operation = Remove
-Type = Path
-Target = usr/lib/modules/*/vmlinuz
-
-[Action]
-Depends = rsync
-Description = Backing up /boot...
-When = PostTransaction
-Exec = /usr/bin/rsync -a --delete /boot /.bootbackup
-EOF
-
-# ZRAM configuration.
-print "Configuring zram."
-cat > /mnt/etc/systemd/zram-generator.conf <<EOF
-[zram0]
-zram-fraction = 1
-max-zram-size = 8192
-EOF
 
 # Configuration of network interfaces through systemd-networkd.
 WIRED_INTERFACE=$(ls /sys/class/net | grep enp)
@@ -308,9 +243,8 @@ EOF
 sed -i "/^#DNS=/cDNS=8.8.8.8" /mnt/etc/systemd/resolved.conf
 
 # Enabling various services.
-print "Enabling automatic snapshots, Btrfs scrubbing, systemd-oomd, and network services."
-for SERVICE in snapper-timeline.timer snapper-cleanup.timer btrfs-scrub@-.timer btrfs-scrub@home.timer btrfs-scrub@var-log.timer btrfs-scrub@\\x2esnapshots.timer grub-btrfs.path systemd-oomd systemd-networkd.service systemd-resolved.service iwd.service
-do
+print "Enabling various services."
+for SERVICE in systemd-boot-update.service systemd-oomd systemd-networkd.service systemd-resolved.service iwd.service; do
     systemctl enable "${SERVICE}" --root=/mnt &>/dev/null
 done
 
@@ -318,3 +252,8 @@ done
 umount -a &>/dev/null
 print "The installer script is done."
 exit
+
+
+# TODO:
+# - removing rsync from this script, so add it to the installation instructions
+# - still need efibootmgr?
